6003d9f624df4535ee5cdebb8e39edcd56f131bc
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-10-22 11:27:36 -0500

    Finished Syntax Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,53 @@ public class HelloWorld
 
         System.out.println("Hello,");
         System.out.println(" World!");
+
+             int myFavoriteNumber = 15;
+             System.out.println(myFavoriteNumber);
+
+             String myString = "This is my string!";
+             System.out.println(myString);
+
+             long myNumber = 123;
+             System.out.println(myNumber);
+
+             double myNumberAgain = 3.14;
+             System.out.println(myNumberAgain);
+
+
+//             int x = 5; //x = 5
+//             System.out.println(x++); // x++ keeps it equal to 5
+//             System.out.println(x); // return as 6
+
+//             int x = 5; // x = 5
+//             System.out.println(++x); // ++x makes it equal to 6
+//             System.out.println(x); // return as 6
+
+//             String theNumberThree = "three";
+//             Object o = theNumberThree;
+//             int three = (int) o; // class java.lang.String cannot be cast to class java.lang.Integer
+
+//             int three = (int) "three"; // java.lang.String cannot be converted to int
+
+//             int x = 4; // x = 4
+//             x = x + 5; // x + 5
+//
+//             System.out.println(x); // Returns 9
+
+//             int x = 3; // x = 3
+//             int y = 4; // y = 4
+//             y = y * x; // y is now = 12
+//             System.out.println(x); // Returns 3
+//             System.out.println(y); // Returns 12
+
+             int x = 10;
+             int y = 2;
+             x = x / y;
+             y = y - x;
+
+             System.out.println(x);
+            System.out.println(y);
+
         }
     }
 
