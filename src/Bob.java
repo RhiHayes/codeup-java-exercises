30a72fd9691eedd1b3ee9590b0e724d480dc8fc9
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-10-26 11:24:12 -0500

    Finished Part 2 Strings Exercise - Bob

---
+++ src/Bob.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bob {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Hey, I'm Bob...ask me whatever, I guess.\n");
+
+
+            String userInput = scanner.nextLine();
+
+            if (userInput.endsWith("?")) {
+                System.out.printf("Sure.");
+            }
+            else if (userInput.endsWith("!")) {
+                System.out.printf("Woah, chill out!");
+            }
+            else if (userInput.isEmpty()) {
+                System.out.printf("Fine, be that way!");
+            } else {
+                System.out.printf("Whatever.");
+            }
+
+    }
+
+}
