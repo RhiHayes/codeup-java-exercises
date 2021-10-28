1e29c893c3932498863de0e5bd4991ba4f209e68
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-10-28 14:25:16 -0500

    Completed Objects Basics

--- src/Person.java
+++ src/Person.java
@@ -1,57 +1,33 @@
 public class Person {
 
-    //A class: This is the template or a blueprint for a 'PERSON'
+    private String name;
 
-    //instance variables
-    public String firstName;
-    public String lastName;
-    public int age;
-
-    //instance method
-
-    public String joinChat() {
-        return lastName + " , " + firstName + " has entered the chat!";
+    public Person(String name){
+        this.name = name;
     }
 
-    //make a static property - representing the world population
+    public String getName(){
+        //TODO: return the person's name
+        return name;
+    }
 
-    public static long worldPop = 7_903_000_000L;
+    public void setName(String name){
+        //TODO: change the name property to the passed value
+        this.name = name;
+    }
 
+    public void sayHello(){
+        //TODO: print a message to the console using the person's name
+        System.out.println("Hello, " + name);
+    }
 
     public static void main(String[] args) {
-        Person rhi = new Person();
 
-        rhi.firstName = "Rhiannon";
-        rhi.lastName = "Hayes";
+        Person rhiannon = new Person("Rhiannon");
 
-        System.out.println("Rhi First Name = " + rhi.firstName);
-        System.out.println("Rhi Last Name = " + rhi.lastName);
-
-        System.out.println(rhi.joinChat());
-
-        System.out.println("World pop = " + worldPop);
-
-        Person douglas = new Person();
-        douglas.firstName = "Douglas";
-        douglas.lastName = "Hirsh";
-
-        //Douglas didn't get counted in the worldPop :0 how would I add him
-        // into our count?
-
-
-        System.out.println("World pop with Douglas :D = " + worldPop);
-        System.out.println(douglas.worldPop); // this WILL run, but it is NOT PROPER CODE
-
-        //Can we...talk to Person.firstName
-
-//        System.out.println(Person.firstName); //CITE - no going forward until we fix this
-
-
-
-
-
-
-
+        rhiannon.sayHello();
+        rhiannon.setName("Kimi");
+        rhiannon.sayHello();
     }
 
 
---
+++ src/PersonLecture.java
@@ -0,0 +1,52 @@
+public class PersonLecture {
+
+    //A class: This is the template or a blueprint for a 'PERSON'
+
+    //instance variables
+    public String firstName;
+    public String lastName;
+    public int age;
+
+    //instance method
+
+    public String joinChat() {
+        return lastName + " , " + firstName + " has entered the chat!";
+    }
+
+    //make a static property - representing the world population
+
+    public static long worldPop = 7_903_000_000L;
+
+
+    public static void main(String[] args) {
+        PersonLecture rhi = new PersonLecture();
+
+        rhi.firstName = "Rhiannon";
+        rhi.lastName = "Hayes";
+
+        System.out.println("Rhi First Name = " + rhi.firstName);
+        System.out.println("Rhi Last Name = " + rhi.lastName);
+
+        System.out.println(rhi.joinChat());
+
+        System.out.println("World pop = " + worldPop);
+
+        PersonLecture douglas = new PersonLecture();
+        douglas.firstName = "Douglas";
+        douglas.lastName = "Hirsh";
+
+        //Douglas didn't get counted in the worldPop :0 how would I add him
+        // into our count?
+
+
+        System.out.println("World pop with Douglas :D = " + worldPop);
+        System.out.println(douglas.worldPop); // this WILL run, but it is NOT PROPER CODE
+
+        //Can we...talk to Person.firstName
+
+//        System.out.println(Person.firstName); //CITE - no going forward until we fix this
+
+    }
+
+
+}
