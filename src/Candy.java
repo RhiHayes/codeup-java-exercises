69d82d383f841f0ee29f3049f289e5fc200bc33c
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-01 09:47:00 -0500

    Finished Warmup 11/1/21

---
+++ src/Candy.java
@@ -0,0 +1,53 @@
+public class Candy {
+
+    public String candyName;
+    public String candySize;
+
+
+    public Candy (String name) {
+
+        this.candyName = name;
+        this.candySize = "idk";
+
+    }
+
+    public Candy (String name, String size) {
+
+        this.candyName = name;
+        this.candySize = size;
+
+    }
+
+
+    public static void main(String[] args) {
+
+//        Java WarmUp
+//        Write a public class named ‘Candy’ - this class should have two public fields,
+//        both taking in a string literal as a value.
+//        The first would be ‘name’ and the second will be ‘size’.
+//        The class will also need two constructors - the first for if we know the name,
+//        but not the size. Your first constructor should set some kind of string literal for the
+//        size.
+//        The second would be for if we know both the name and the size. This second constructor
+//        will pass through those arguments into the Candy object being instantiated.
+
+//        BONUS:
+//        Write a method for your Candy class - this method should print out a message saying:
+//“Ahhh, a [name]! I see that you got the [size] of it. Sounds good! :)”
+
+        Candy reeses = new Candy("Reeses");
+        Candy hershey = new Candy("Hershey's", "Big Bar");
+
+        System.out.println(reeses.candyName + " " + reeses.candySize);
+        System.out.println(hershey.candyName + " " + hershey.candySize);
+
+
+
+    }
+
+
+
+
+
+
+}
