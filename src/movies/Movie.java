2c79569114ca9a8c4481b5100ccd7bb0f2164630
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2021-11-01 16:44:17 -0500

    Arrays Exercises - Working on Exercise 3

---
+++ src/movies/Movie.java
@@ -0,0 +1,21 @@
+package movies;
+
+import java.util.Arrays;
+
+public class Movie {
+
+    private String name;
+    private String category;
+
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,15 @@
+package movies;
+
+import util.Input;
+
+public class MoviesApplication {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+}
